5d4f70aa91ed8838a802120796e6ec8f2a754177
sublimeS0 <sublimee.S0@gmail.com>
2024-11-28 13:36:16 -0500

    Updated proof-of-concept to include modifying game messages

--- src/client/java/com/radonchat/RadonChatClient.java
+++ src/client/java/com/radonchat/RadonChatClient.java
@@ -25,13 +25,52 @@ public class RadonChatClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		LOGGER.info("register test");
+		LOGGER.info("Initializing radon-chat client...");
 
+		// Register to basic chat messages
 		ClientReceiveMessageEvents.ALLOW_CHAT.register((message, signedMessage, sender, params, receptionTimestamp) -> {
-			MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.empty().setStyle(message.getStyle()).append(message).append(" - Radon"));
-
-			return message.getString().contains("Radon");
-
+			MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(styleNameInMessage(message));
+			return false; // Block original message in favor of styled
 		});
+
+		// Register to game messages (for servers that intercept and re-send player messages)
+		ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> {
+			if(!isMessageFromPlayer(message.getString())) {
+				return true;
+			}
+
+			MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(styleNameInMessage(message));
+			return false;
+		});
+	}
+
+	/**
+	 *
+	 * @param message
+	 * @return
+	 */
+	private Text styleNameInMessage(Text message) {
+		final Style initStyle = message.getStyle();
+		final Style nameStyle = message.getStyle().withColor(TextColor.parse("#2030FE").getOrThrow());
+
+		final String messageString = message.getString();
+
+		final String senderName = messageString.substring(messageString.indexOf("<") + 1, messageString.indexOf(">"));
+
+		final Text pre = Text.empty().setStyle(initStyle).append("<");
+		final Text styledName = Text.empty().setStyle(nameStyle).append(senderName);
+		final Text contents = Text.empty().setStyle(initStyle).append(">").append(messageString.substring(messageString.indexOf(">") + 1));
+
+		return Text.empty().append(pre).append(styledName).append(contents);
+	}
+
+	/**
+	 * Detects (kinda) if the message is from a player
+	 *
+	 * @param message - Chat message as string
+	 * @return - True if the message is from a player, false otherwise
+	 */
+	private boolean isMessageFromPlayer(String message) {
+		return message.contains("<") && message.contains(">");
 	}
 }
